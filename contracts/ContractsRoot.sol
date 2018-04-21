85d9d75b7ce49f89a2b26aacbb7ea40fc5be9efe
Dima Stadub <dev@eml.by>
2018-04-21 08:30:43 +0300

    contract template

---
+++ contracts/ContractsRoot.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.4;
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract ContractsRoot is Ownable {
+  function ContractsRoot() {
+    // constructor
+  }
+}
---
+++ contracts/Registration.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.4;
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract Registration is Ownable {
+  struct Relationship{
+    string Man;
+    string Wife;
+  }
+
+  mapping (uint=>Relationship) registrations;
+  uint curIndex;
+  function Registration() {
+    // constructor
+  }
+
+  function register(string id, string id2) public returns(uint index){
+      index = curIndex++;
+      registrations[index] = Relationship({Man:id,Wife:id2});
+  }
+   
+  function getByIndex(uint index) public constant returns (string,string){
+      return (registrations[index].Man, registrations[index].Wife);
+  }
+}
